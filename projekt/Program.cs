b0fab40c829b2648ddf4104bf91c388c683fb967
EmilWallett <nhlpool27@gmail.com>
2022-08-29 10:43:51 +0200

    lagt till project

---
+++ projekt/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Är det fint väder?");
+string s = Console.ReadLine();
+bool svar = s == "ja";
+bool svar1 = s =="Ja";
+bool svar2 = s =="nej";
+bool svar3 = s =="Nej";
+
+if(svar)
+{
+    Console.WriteLine("VI går på picknick!");
+}
+else if(svar1)
+{
+    Console.WriteLine("Vi går på picknick!");
+}
+else if(svar2)
+{
+    Console.WriteLine("Vi stannar inne och läser.");
+}
+else if(svar3)
+{
+    Console.WriteLine("Vi stannar inne och läser.");
+}
+else
+{
+    Console.WriteLine("Jag föstår inte.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
